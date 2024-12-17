12718ba2b51cea0ee21fd9b6f79e691344eae533
levitpringle <levitpringle@gmail.com>
2024-12-16 16:35:05 -0800

    Gulpfile buildStyles function

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,21 @@
 'use strict';
 
 const gulp = require('gulp');
-const sass = require('gulp-sass');
-const browserSync = require('browser-sync').create();
-const runSequence = require('run-sequence');
+const sass = require('gulp-sass')(require('sass'));
+//const browserSync = require('browser-sync').create();
+//const runSequence = require('run-sequence');
 
-gulp.task('sass', function() {
-  return gulp.src('web/assets/sass/*.scss')
+//gulp.src('web/assets/sass/*.scss').pipe(sass().on('error', sass.logError)).pipe(gulp.dest('web/assets/css')).pipe(browserSync.reload({stream: true}));
+
+function buildStyles() {
+  return gulp.src("./web/assets/sass/main.scss")
   .pipe(sass().on('error', sass.logError))
-  .pipe(gulp.dest('web/assets/css'))
-  .pipe(browserSync.reload({
-    stream: true
-  }));
-});
+  .pipe(gulp.dest("./web/assets/css"));
+}
 
-gulp.task('browserSync', function() {
-  browserSync.init({
-    proxy:'ecommerce.ddev.site'
-  });
-});
-
-gulp.task('watch',['browserSync', 'sass'], function() {
-  gulp.watch('web/assets/sass/*.scss', ['sass']);
-  gulp.watch('templates/*.twig', browserSync.reload); 
-});
-
-//Default
-gulp.task('default', function (callback) {
-  runSequence(['sass', 'browserSync', 'watch'],
-    callback
-  );
-});
+exports.buildStyles = buildStyles;
+//Watch not working*
+exports.watch = function () {
+  gulp.watch('./web/assets/sass/*.scss', buildStyles);
+};
+exports.default = exports.watch;
